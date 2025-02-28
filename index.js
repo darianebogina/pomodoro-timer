95f4bdeb00b64e7b5c374ca7a2d5ad21d93f5c0f
unknown <dasha.nebogina@mail.ru>
2025-02-28 17:22:45 +0300

    feat: implement stop and reset buttons

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const slider = document.getElementById("slider");
 const textTime = document.getElementById("time-text");
 const startBtn = document.getElementById("start-button");
 slider.addEventListener("click", setTime);
+let timer;
 
 function setTime() {
     textTime.innerHTML = slider.value + ":00";
@@ -11,13 +12,30 @@ function startTimer() {
     let timeParts = textTime.textContent.split(":");
     let minutes = Number(timeParts[0]);
     let seconds = Number(timeParts[1]);
-
     let currentTime = minutes * 60 + seconds;
-    let timer = setInterval(() => {
+    
+    startBtn.disabled = true;
+
+    timer = setInterval(() => {
         currentTime--;
         minutes = Math.floor(currentTime / 60);
         seconds = currentTime % 60;
         textTime.textContent = minutes + ":" + seconds;
+        if (currentTime <= 0) {
+            clearInterval(timer);
+            startBtn.disabled = false;
+        }
     },
-    1000);
+        10);
+}
+
+function stopTimer() {
+    clearInterval(timer);
+    startBtn.disabled = false;
+}
+
+function resetTimer() {
+    clearInterval(timer);
+    textTime.innerHTML = slider.value + ":00";
+    startBtn.disabled = false;
 }
